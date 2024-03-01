ba9315d514b51fa22cccd885c7e50a7c449ceccb
MichalWoda <michalwoda@gmail.com>
2024-03-01 14:07:26 +0100

    Dodano druga funkcje, wyjatki oraz drobne poprawki

--- R/th_calc.R
+++ R/th_calc.R
@@ -1,39 +1,49 @@
 #' Top Height (TH) calculation
 #'
-#' @description Calculate TH of the tree stand in 10x10 m raster. Using
-#' 1/3 highest cell values
+#' @description Calculate TH of the tree stand in 10x10 m raster. Using two optional function
+#' to obtain top height.
 #'
+#' @param chm canopy height model (resolution equal or higher than 1 meter).
+#' SpatRaster or RasterLayer
+#' @param f function used to calculate top height. Build-in functions: "h_23","h_sd"
 #'
-#' @param chm canopy height model (resolution equal or lower than 1 m)
-
+#' @details
+#' - "h_23" - (default) mean of 1/3 highest cells to aggregate
+#' - "h_sd" - mean of cells higher than threshold: (percentile of 97 - 6.42)
+#'
 #' @return Top Height in the 10x10 m raster
 #' @export
 #'
 #' @examples
-#'r =  terra::rast(ncol=100, nrow=100, nlyr=1, res = 0.5)
-#'rv = runif(terra::ncell(r), min = 5, max = 36)
-#'terra::values(r) = rv
-#'terra::plot(r)
-#'th = th_calc(r)
-#'terra::plot(th)
+#' chm = terra::rast(system.file('raster/chm.tif', package = 'growthmodels'))
+#' terra::plot(chm)
+#' th = th_calc(chm, f = "h_sd")
+#' terra::plot(th)
 
+th_calc = function(chm, f="h_23") {
 
-th_calc = function(chm){
+  # input check
+  if (!inherits(chm, c("SpatRaster","RasterLayer"))) stop("Parameter chm is not a valid datatype")
 
+  if (inherits(chm, "RasterLayer")) chm <- terra::rast(chm)
 
-  chm_1 = terra::aggregate(chm, fact=1/terra::xres(chm), fun=mean, na.rm = T)
-  rm(chm)
-  chm_10 = terra::aggregate(chm_1, fact=10, fun=mean, na.rm = T)
+  # default functions list and check of f parameter
+  f_list <- list(
+    "h_23" = "function(x) mean(na.omit(x)[na.omit(x)>(mean(na.omit(x))*2/3)])",
+    "h_sd" = "function(x) mean(na.omit(x)[na.omit(x)>(quantile(na.omit(x),.97)-6.42)])"
+  )
+  if (all(is.character(f) & !f%in%names(f_list))) stop("Name of the function parameter is not valid")
 
-  chm_10 = terra::resample(chm_10, chm_1)
-  chm_23 = chm_1
+  # resolution check
+  if (terra::xres(chm) < 1) {
+    chm = terra::aggregate(chm, fact=1, fun=mean, na.rm = T)
+  }
+  else if (terra::xres(chm) > 1) {
+    stop("Require raster resolution is [1] meter or higher")
+  }
 
-  chm_23[chm_23<(2/3)*chm_10] = NA
-  th = terra::aggregate(chm_23, fact=10, fun=mean, na.rm = T)
-  rm(chm_1, chm_10, chm_23)
-
+  th <- terra::aggregate(chm,
+                         fact=10,
+                         fun=eval(parse(text = paste(f_list[f]))))
   return(th)
-
 }
-
-
