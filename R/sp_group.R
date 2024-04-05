498080453e9cf093425d7613310a55d5af5d6359
imie nazwiskoPiotrJaniec <piotrjaniec2@gmail.com>
2024-04-05 16:33:35 +0200

    Do funkcji sp_group dodano funkcjonalność grupowania wg. trzech kategorii (height, volume, growth) podobieństwa gatunków

--- R/sp_group.R
+++ R/sp_group.R
@@ -5,7 +5,9 @@
 #'
 #'
 #' @param species vector of species
+#' @param  type Grouping type (3 options): GRP_H (height group), GRP_V (volume group), GRP_P (growth group). Deafult: GRP_V description
 #' @return Species group name
+#'
 #' @export
 #'
 #' @examples
@@ -14,11 +16,25 @@
 
 
 
-sp_group = function(species){
+sp_group = function(species, type = 'GRP_V'){
   species = data.frame(species)
-  sp_g = readr::read_rds(system.file('sp_dict/sp_groups.rds', package = 'growthmodels'))
-  #sp_g = utils::read.csv2('inst/sp_dict/sp_groups.csv')
-  species = species %>% dplyr::left_join(sp_g, by = c('species' = 'SPECIES_CD'))
+  sp_g = data.table::fread(system.file('sp_dict/sp_groups.csv', package = 'growthmodels')) %>%
+  #sp_g = data.table::fread('inst/sp_dict/sp_groups.csv') %>%
+    dplyr::select('GAT', type)
+
+  species = species %>% dplyr::left_join(sp_g, by = c('species' = 'GAT')) %>%
+    dplyr::rename('species_group' = type)
+
+
+  if(type == 'GRP_V'){
+    message('Grouped by volume')
+  }else if(type == 'GRP_H'){
+    message('Grouped by height')
+  }else{
+    message('Grouped by growth')
+  }
 
   return(species$species_group)
 }
+
+
--- raw-data/gat_dict.R
+++ raw-data/gat_dict.R
@@ -1,13 +1,6 @@
 library(dplyr)
+library(data.table)
 
 
-df = read.csv2('D:/TaksPlot/Portable/gat_dic.csv') %>%
-  distinct(SPECIES_CD, .keep_all = T) %>%
-  select(SPECIES_CD, Grupa_gat) %>%
-  mutate(Grupa_gat = ifelse(SPECIES_CD == 'MD', 'MD', Grupa_gat),
-         Grupa_gat = ifelse(Grupa_gat == 'TP', 'BRZ', Grupa_gat),
-         Grupa_gat = ifelse(Grupa_gat == 'JS', 'BK', Grupa_gat)
-         ) %>%
-  rename(species_group = Grupa_gat)
-
+df = fread('D:/funkcja_bruchwald/SL_GAT.csv')
 write.csv2(df, 'inst/sp_dict/sp_groups.csv')
