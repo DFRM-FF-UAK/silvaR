16ec60bd8e1d8102c98843cc0a30194bb9845285
imie nazwiskoPiotrJaniec <piotrjaniec2@gmail.com>
2024-06-25 13:27:04 +0200

    An option to group species according to your own criterion has been added to the sp_group function for species that do not have groups in the dictionary.

--- R/sp_group.R
+++ R/sp_group.R
@@ -1,11 +1,12 @@
 #' Group species
 #'
-#' @description Group species in eight groups (BK, OL, MD, BRZ, DB, ŚW, JD, SO)
+#' @description Group species in eight (BK, OL, MD, BRZ, DB, ŚW, JD, SO) or more groups
 #' necessary for the height and growth prediction
 #'
 #'
 #' @param species vector of species
 #' @param  type grouping type (5 options: GRP_P, GRP_TH, GRP_V, GRP_H, GRP_P_BDL)
+#' @param others how to group species without a group (deafult BK)
 #'
 #' @details
 #' - GRP_P - (default) growth group
@@ -14,16 +15,17 @@
 #' - GRP_H - height group; Forest Data Bank based
 #' - GRP_P_BDL - growth group; Forest Data Bank based
 #'
+#'
 #' @return Species group name
 #'
 #' @export
 #'
 #' @examples
-#' species_list = c('AK', 'DB', 'CZM.P')
+#' species_list = c('AK', 'DB', 'WZ.G')
 #' sp_group(species_list, 'GRP_TH')
 
 
-sp_group = function(species, type = 'GRP_P'){
+sp_group = function(species, type = 'GRP_P', others = "BK"){
 
   # check type
   stopifnot("Group type is not valid" = type %in% c('GRP_H','GRP_TH','GRP_V','GRP_P','GRP_P_BDL'))
@@ -34,18 +36,24 @@ sp_group = function(species, type = 'GRP_P'){
     dplyr::select('GAT', all_of(type))
 
   # omit empty group value
-  sp_g = sp_g[!sp_g[[type]] %in% c(NA, "", " "),]
+  #sp_g = sp_g[!sp_g[[type]] %in% c(NA, "", " "),]
 
   # compare input species with dictionary
   if (!all(species$species %in% unique(sp_g$GAT))) {
-    stop(paste0("Some species: ",
+    warning(paste0("Some species: ",
                 paste(species$species[!species$species %in% unique(sp_g$GAT)], collapse = ";"),
-                " are not present in the dictionary")
+                " are not present in the dictionary we grouped it as:", others)
     )
   }
 
+
   species = species %>% dplyr::left_join(sp_g, by = c('species' = 'GAT')) %>%
     dplyr::rename('species_group' = type)
+
+  # fill in species without a group
+
+  species = species %>%
+    dplyr::mutate(species_group := dplyr::if_else(is.na(species_group) | species_group %in% c("", " "), others, species_group))
 
 
   if (type == 'GRP_V') {
@@ -62,3 +70,4 @@ sp_group = function(species, type = 'GRP_P'){
 
   return(species$species_group)
 }
+
