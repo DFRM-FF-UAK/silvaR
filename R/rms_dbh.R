5e9a30fd19b918409145d069beeaccd34393ef5a
MichalWoda <michalwoda@gmail.com>
2025-12-29 20:35:46 +0100

    function corrections

--- R/rms_dbh.R
+++ R/rms_dbh.R
@@ -1,34 +1,49 @@
 #' Calculate RMS dbh at the sample plot
 #'
-#' @description Calculate RMS dbh of the species at the plot in the layer and age group
+#' @description Calculate root mean square (RMS) diameter at breast height (DBH)
+#'  of the species at the plot in the layer and age group.
 #'
+#' @param plot_id Integer or factor. Unique plot identifier.
+#' @param tree_id Integer or factor. Unique tree identifier within plot.
+#' @param species Character. Tree species code.
+#' @param age Integer. Tree age.
+#' @param layer Integer. Stand structure layer.
+#' @param dbh Numeric. Diameter at breast height (cm).
+#' @param only_measured_h Logical. If TRUE, only trees with measured height
+#'   are used. Default is TRUE.
+#' @param height Numeric. Tree height (m). Required if `only_measured_h = TRUE`.
 #'
-#' @param plot_id Unique plot id
-#' @param tree_id Unique tree id at the plot
-#' @param species Tree species
-#' @param age Tree age
-#' @param layer Stand structure layer
-#' @param dbh Tree dbh
-#' @param only_measured_h if TRUE only trees, with measured dbh will be used (logical, defult TRUE)
-#' @param height Tree heighs (you have to provide tree heights if only_measured_h = T)
-#' @return Average tree dbh
+#' @return
+#' Numeric vector of RMS DBH values corresponding to input rows.
+#'
 #' @export
 #'
 #' @examples
 #' plot_id = c(1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2)
-#' tree_id = c(1, 2 ,3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14)
-#' species = c('SO', 'SO', 'DB', 'JW', 'BK', 'DB', 'DB', 'ŚW', 'ŚW', 'ŚW', 'SO', 'SO', 'SO', 'SO')
+#' tree_id = c(1, 2 ,3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 7, 8)
+#' species = c('SO', 'SO', 'DB', 'JW', 'BK', 'DB',
+#'             'DB', 'ŚW', 'ŚW', 'ŚW', 'SO', 'SO', 'SO', 'SO')
 #' age = c(40, 40, 40, 40, 60, 45, 50, 50, 50, 60, 60, 60, 60, 60)
 #' layer = c(1, 1, 2, 2, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1)
 #' height = c(21, 13, 24, NA, 12, NA, 18, NA, NA, 31, 32, 24, 25, NA)
 #' dbh = c(33, 32, 31, NA, 28, 47, 12, 22, 41, 48, 27, 42, 25, 33)
-#' av_dbh(plot_id, tree_id, species, age, layer, dbh, height)
+#'
+#' rms_dbh(plot_id, tree_id, species, age, layer, dbh, height)
 
+rms_dbh = function(plot_id,
+                   tree_id,
+                   species,
+                   age,
+                   layer,
+                   dbh,
+                   height,
+                   only_measured_h = T) {
 
+  if (!is.logical(only_measured_h) || length(only_measured_h) != 1) {
+    stop("`only_measured_h` must be a single logical value.")
+  }
 
-rms_dbh = function(plot_id, tree_id, species, age, layer, dbh, height, only_measured_h = T){
-
-  if (only_measured_h == T) {
+  if (only_measured_h) {
 
     if (missing(height)) {
       stop("Please provide height parameter or change only_measured_h to FALSE")
@@ -47,14 +62,15 @@ rms_dbh = function(plot_id, tree_id, species, age, layer, dbh, height, only_meas
       dplyr::distinct()
 
     empty_d = df_fil[is.na(df_fil$DBH), ]
+
     if(nrow(empty_d) > 0) {
       warning(paste("Sorry, but we couldn't calculate average dbh for these groups:\n",
                     paste(paste0('Plot ', unique(empty_d$plot_id),': ', unique(empty_d$species), ' ', unique(empty_d$layer)), collapse = "\n"),
                     '\n No dbh or height measurements'))
     }
 
+  } else {
 
-  }else{
     df = data.frame(plot_id, species, age, layer, dbh) %>%
       dplyr::group_by(plot_id, species, age, layer) %>%
       dplyr::mutate(DBH = mean(dbh)) %>%
@@ -68,8 +84,6 @@ rms_dbh = function(plot_id, tree_id, species, age, layer, dbh, height, only_meas
                     '\n No dbh measurements'))
     }
   }
-
-
 
   return(df$DBH)
 }
